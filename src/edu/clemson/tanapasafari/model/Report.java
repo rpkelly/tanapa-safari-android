e5384f0594374aad21fe88d375dbecfe95ddfb39
Michael Vaughan <jmvaugh@clemson.edu>
2014-04-07 10:43:18 -0400

    Implemented the local database. Also implemented the synchronization service that periodocially checks the local database for unsynchronized reports and attempts to upload them.

--- src/edu/clemson/tanapasafari/model/Report.java
+++ src/edu/clemson/tanapasafari/model/Report.java
@@ -2,6 +2,12 @@ package edu.clemson.tanapasafari.model;
 
 import java.util.Date;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+import edu.clemson.tanapasafari.constants.Constants;
+
 public class Report {
 
 	private long id;
@@ -86,6 +92,28 @@ public class Report {
 		this.synch = synch;
 	}
 	
-	
+	public JSONObject toJSON() {
+		
+		JSONObject reportJson = new JSONObject();
+		
+		try {
+			reportJson.put("id", this.getId());
+			reportJson.put("report_type_id", this.getReportTypeId());
+			reportJson.put("content", this.getContent());
+			reportJson.put("time", Constants.ISO_8601_DATE_FORMAT.format(this.getTime()));
+			reportJson.put("latitude", this.getLatitude());
+			reportJson.put("longitude", this.getLongitude());
+			reportJson.put("user_id", this.getUserId());
+			if (this.media != null) {
+				//reportJson.put("media", media.toJSON());
+			}
+			reportJson.put("synch", this.synch);
+		} catch (JSONException e) {
+			Log.e(Constants.LOGGING_TAG, "Error occurred while serializing User object to JSON.", e);
+		}
+		
+		return reportJson;
+		
+	}
 	
 }
